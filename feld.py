08073f190804656c235950885618c6b684da0605
JamesBond <117627691+bond120@users.noreply.github.com>
2025-06-29 19:32:04 +0200

    Update feld.py

--- feld.py
+++ feld.py
@@ -10,7 +10,7 @@ intents.guilds = True
 intents.messages = True
 intents.guild_messages = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
